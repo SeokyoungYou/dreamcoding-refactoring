c9caafdc145d8b2df298d6858e5f0ebd7f1ecd62
Quartz <skyu.dev@gmail.com>
2022-12-20 11:35:56 +0900

    challenge: refactor 6-11 by myself

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,30 +1,97 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+  // const basePrice = product.basePrice * quantity;
+
+  // const discount =
+  //   Math.max(quantity - product.discountThreshold, 0) *
+  //   product.basePrice *
+  //   product.discountRate;
+
+  const totalBasePrice = product.getTotalBasePrice(quantity);
+  const discount = product.getTotalDiscount(quantity);
+
+  // const shippingPerCase = getShippingPerCase(totalBasePrice);
+  // totalBasePrice > shippingMethod.discountThreshold
+  //   ? shippingMethod.discountedFee
+  //   : shippingMethod.feePerCase;
+  const shippingCost = shippingMethod.getShippingCost(quantity, totalBasePrice);
+  const price = totalBasePrice - discount + shippingCost;
   return price;
 }
 
+class Product {
+  #basePrice;
+  #discountRate;
+  #discountThreshold;
+  constructor(data) {
+    this.#basePrice = data.basePrice;
+    this.#discountRate = data.discountRate;
+    this.#discountThreshold = data.discountThreshold;
+  }
+  get basePrice() {
+    return this.#basePrice;
+  }
+  get discountRate() {
+    return this.#discountRate;
+  }
+  get discountThreshold() {
+    return this.#discountThreshold;
+  }
+
+  getTotalBasePrice(quantity) {
+    return this.#basePrice * quantity;
+  }
+
+  getTotalDiscount(quantity) {
+    return (
+      Math.max(quantity - this.#discountThreshold, 0) *
+      this.#basePrice *
+      this.#discountRate
+    );
+  }
+}
+class ShippingMethod {
+  #discountThreshold;
+  #feePerCase;
+  #discountedFee;
+  constructor(data) {
+    this.#discountThreshold = data.discountThreshold;
+    this.#feePerCase = data.feePerCase;
+    this.#discountedFee = data.discountedFee;
+  }
+  get discountThreshold() {
+    return this.#discountThreshold;
+  }
+  get feePerCase() {
+    return this.#feePerCase;
+  }
+  get discountedFee() {
+    return this.#discountedFee;
+  }
+
+  getShippingPerCase(totalBasePrice) {
+    return totalBasePrice > this.#discountThreshold
+      ? this.#discountedFee
+      : this.#feePerCase;
+  }
+
+  getShippingCost(quantity, totalBasePrice) {
+    const shippingPerCase = this.getShippingPerCase(totalBasePrice);
+    return quantity * shippingPerCase;
+  }
+}
+
 // 사용 예:
-const product = {
+const product = new Product({
   basePrice: 10,
   discountRate: 0.1,
   discountThreshold: 10,
-};
+});
 
-const shippingMethod = {
+const shippingMethod = new ShippingMethod({
   discountThreshold: 20,
   feePerCase: 5,
   discountedFee: 3,
-};
+});
 
 const price = priceOrder(product, 5, shippingMethod);
 console.log(price);
