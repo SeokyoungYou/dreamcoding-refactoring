149dbc93d95251fb54131dc907a146e35a8a6054
Quartz <skyu.dev@gmail.com>
2023-01-02 18:18:07 +0900

    lecture: complete

--- challenge/createStatement.js
+++ challenge/createStatement.js
@@ -1,62 +1,84 @@
 class Performance {
   #audience;
   #play;
-  #amount;
   constructor(audience, play) {
     this.#audience = audience;
     this.#play = play;
   }
+
+  get play() {
+    return this.#play;
+  }
+
   get audience() {
     return this.#audience;
   }
-  get play() {
-    return this.play;
-  }
-  get credits() {
-    let result = Math.max(this.#audience - 30, 0);
-    if ("comedy" === this.#play.type) {
-      result += Math.floor(this.#audience / 5);
+
+  static create(audience, play) {
+    switch (play.type) {
+      case "tragedy":
+        return new Tragedy(audience, play);
+      case "comedy":
+        return new Comedy(audience, play);
+      default:
+        throw new Error(`알수없는 타입: ${play.type}`);
     }
+  }
+}
+
+class Tragedy extends Performance {
+  get amount() {
+    const base = 40000;
+    return this.audience > 30 ? base + 1000 * (this.audience - 30) : base;
+  }
+
+  get credits() {
+    return Math.max(this.audience - 30, 0);
+  }
+}
+
+class Comedy extends Performance {
+  get amount() {
+    let result = 30000;
+    if (this.audience > 20) {
+      result += 10000 + 500 * (this.audience - 20);
+    }
+    result += 300 * this.audience;
     return result;
   }
 
-  get amount() {
-    let result = 0;
-    switch (this.#play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (this.#audience > 30) {
-          result += 1000 * (this.#audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (this.#audience > 20) {
-          result += 10000 + 500 * (this.#audience - 20);
-        }
-        result += 300 * this.#audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.#play.type}`);
-    }
-    return result;
+  get credits() {
+    return Math.max(this.audience - 30, 0) + Math.floor(this.audience / 5);
+  }
+}
+
+class Statement {
+  #customer;
+  #performances;
+  constructor(invoice, plays) {
+    this.#customer = invoice.customer;
+    this.#performances = invoice.performances.map((p) =>
+      Performance.create(p.audience, plays[p.playID])
+    );
+  }
+
+  get customer() {
+    return this.#customer;
+  }
+
+  get performances() {
+    return [...this.#performances];
+  }
+
+  get totalAmount() {
+    return this.#performances.reduce((sum, p) => (sum += p.amount), 0);
+  }
+
+  get totalCredits() {
+    return this.#performances.reduce((sum, p) => (sum += p.credits), 0);
   }
 }
 
 export function createStatement(invoice, plays) {
-  const statement = {};
-  statement.customer = invoice.customer;
-  statement.performances = invoice.performances.map(
-    (p) => new Performance(p.audience, plays[p.playID])
-  );
-  statement.totalAmount = totalAmount(statement.performances);
-  statement.totalCredits = totalCredits(statement.performances);
-  return statement;
-
-  function totalCredits(performances) {
-    return performances.reduce((sum, p) => (sum += p.credits), 0);
-  }
-  function totalAmount(performances) {
-    return performances.reduce((sum, p) => (sum += p.amount), 0);
-  }
+  return new Statement(invoice, plays);
 }
--- challenge/statement.js
+++ challenge/statement.js
@@ -1,28 +1,28 @@
-import { createStatement } from "./createStatement";
+import { createStatement } from "./create_statement";
 
 export function statement(invoice, plays) {
-  return rednerPlainText(createStatement(invoice, plays));
+  return renderPlainText(createStatement(invoice, plays));
 }
 
-function htmlStatement(invoice, plays) {
-  return rednerHTML(createStatement(invoice, plays));
+export function htmlStatement(invoice, plays) {
+  return renderHTML(createStatement(invoice, plays));
 }
 
-function rednerHTML(statement) {
+function renderHTML(statement) {
   let result = `<h1>청구 내역 (고객명: ${statement.customer})</h1>\n`;
-
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>석</th><th>cost</th></tr>";
   for (let perf of statement.performances) {
-    result += `  ${perf.play.name}: ${usd(perf.amount / 100)} (${
-      perf.audience
-    }석)\n`;
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount / 100)}</td></tr>\n`;
   }
-
-  result += `총액: ${usd(statement.totalAmount / 100)}\n`;
-  result += `적립 포인트: ${statement.totalCredits}점\n`;
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(statement.totalAmount / 100)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${statement.totalCredits}</em>점</p>\n`;
   return result;
 }
 
-function rednerPlainText(statement) {
+function renderPlainText(statement) {
   let result = `청구 내역 (고객명: ${statement.customer})\n`;
 
   for (let perf of statement.performances) {
@@ -43,7 +43,6 @@ function usd(number) {
     minimumFractionDigits: 2,
   }).format(number);
 }
-
 // 사용예:
 const playsJSON = {
   hamlet: { name: "Hamlet", type: "tragedy" },
@@ -79,5 +78,5 @@ const expected =
   "  Othello: $500.00 (40석)\n" +
   "총액: $1,730.00\n" +
   "적립 포인트: 47점\n";
-console.log(result);
+console.log(htmlStatement(invoicesJSON[0], playsJSON));
 console.log(result === expected);
