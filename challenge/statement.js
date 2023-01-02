0367cf100f59c474f242e24ad881e6dac21cd95f
Quartz <skyu.dev@gmail.com>
2023-01-02 17:15:11 +0900

    lecture: detach statement logic to another file

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,61 +1,11 @@
+import { createStatement } from "./createStatement";
+
 export function statement(invoice, plays) {
-  const statement = {};
-  statement.customer = invoice.customer;
-  statement.performances = invoice.performances.map(enrichPerformance);
-  statement.totalAmount = totalAmount(statement.performances);
-  statement.totalCredits = totalCredits(statement.performances);
-  return rednerPlainText(statement, plays);
-
-  function enrichPerformance(performance) {
-    const result = { ...performance };
-    result.play = playFor(performance);
-    result.amount = amountFor(result);
-    result.credits = creditsFor(result);
-    return result;
-  }
-
-  function playFor(performance) {
-    return plays[performance.playID];
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-    return result;
-  }
-  function creditsFor(performance) {
-    let result = Math.max(performance.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === performance.play.type) {
-      result += Math.floor(performance.audience / 5);
-    }
-    return result;
-  }
-  function totalCredits(performances) {
-    return performances.reduce((sum, p) => (sum += p.credits), 0);
-  }
-  function totalAmount(performances) {
-    return performances.reduce((sum, p) => (sum += p.amount), 0);
-  }
+  const statement = createStatement(invoice, plays);
+  return rednerPlainText(statement);
 }
 
-function rednerPlainText(statement, plays) {
+function rednerPlainText(statement) {
   let result = `청구 내역 (고객명: ${statement.customer})\n`;
 
   for (let perf of statement.performances) {
